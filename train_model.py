5e4afe9d6d5aeb1c7172db8af5e827c4eac38497
michae-lzhou <ziyang.zhou@gmail.com>
2024-12-14 21:18:56 -0500

    .

--- train_model.py
+++ train_model.py
@@ -11,7 +11,7 @@ data_yaml_path = os.path.join(script_dir, "data.yaml")
 # Train the model with explicit dataset path
 results = model.train(
     data=data_yaml_path,
-    epochs=300,
+    epochs=100,
     project="dataset/checkpoints",
     name="small-Model",
     batch=32,
